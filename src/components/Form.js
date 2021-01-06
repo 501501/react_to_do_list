2bcd9226810a69f4679f2dea7cf8a5a107c4889c
501501 <76206286+501501@users.noreply.github.com>
2021-01-06 17:34:15 +0900

    Update Form.js
    #1.1 modify add button error

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,7 @@ const Form=({value, onChange, onCreate, onKeyPress}) => {
     return (
         <div className="form">
             <input value={value} onChange={onChange} onKeyPress={onKeyPress}/>
-            <div className="create-button" onClick={onclick}>
+            <div className="create-button" onClick={onCreate}>
                 +
             </div>
         </div>
